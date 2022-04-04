ff083f33435a4dfee0c7611c8b9c1bc147482d49
muhammedrishadpe <rishadpe@gmail.com>
2022-04-04 20:50:19 +0530

    Storing Orders in the web

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products_provider.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  UserProductItem(
+    this.id,
+    this.title,
+    this.imageUrl,
+  );
+  @override
+  Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  EditProductScreen.routeName,
+                  arguments: id,
+                );
+              },
+              icon: Icon(
+                Icons.edit,
+              ),
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              onPressed: () async {
+                try {
+                  await Provider.of<ProductsProvider>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (errot) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        "Deleting failed!",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }
+              },
+              icon: Icon(
+                Icons.delete,
+              ),
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
